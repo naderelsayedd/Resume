aa91a76b9ff49dba57224df107fa505ec10a6da0
naderelsayedd <elsayednder@gmail.com>
2023-09-19 11:03:13 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(".info1").click(function() {
+    $(".p1").toggle();
+})
+$(".info2").click(function() {
+    $(".p2").toggle();
+})
+$(".info3").click(function() {
+    $(".p3").toggle();
+})
+$(".info4").click(function() {
+    $(".p4").toggle();
+})
